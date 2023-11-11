8d5f8701a787b64c10285daafe07995b16f3494a
Crowdbotics <team@crowdbotics.com>
2023-11-11 04:04:10 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "vip-vpn-44481",
+  url: "https://vip-vpn-44481.botics.co",
+  api: "https://vip-vpn-44481.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
